9e788a1912ccd6f69833b80daf1bcaac69671e7f
Dmitriy Makarov <makarov.dmitriy@gmail.com>
2016-09-03 07:30:56 +0000

    Add 'test' class

---
+++ Test.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Symfony\Component\MyTest;
+
+/**
+ * Class Test
+ * @package Symfony\Component\MyTest
+ */
+class Test
+{
+    /**
+     * Returns 'test' string
+     * @return string
+     */
+    public function test()
+    {
+        return 'test';
+    }
+}
